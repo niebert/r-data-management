90cca325f53bd080cb5d2be029c21f0defba4962
Engelbert Niehaus <niehausbert@gmail.com>
2021-10-18 15:31:44 +0200

    Create 1_used_libraries.r

---
+++ src/1_used_libraries.r
@@ -0,0 +1,5 @@
+library(knitr)
+library(readr) # Linux installation has some issues - read.csv() and write.csv as alternative?
+library(lubridate) # necessary for date handling
+library(dplyr)
+library(ggplot2)
